fe15aecc7f43d0d4d13ab8da6f92360d2d121a54
renan-77 <renanmonteiroft@gmail.com>
2021-03-02 14:03:59 +0000

    add Forms converted into single component

--- frontend/src/app/add/add.component.ts
+++ frontend/src/app/add/add.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
 import {DataService} from '../data.service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 
 @Component({
@@ -10,25 +10,75 @@ import {Router} from '@angular/router';
     styleUrls: ['./add.component.css']
 })
 export class AddComponent implements OnInit {
+    whichForm;
+
     personForm: FormGroup;
 
-    constructor(private dataService: DataService, private router: Router) { }
+    managerForm;
+
+    salesmanForm;
+
+    constructor(private dataService: DataService, private router: Router, private route: ActivatedRoute) { }
 
     genders;
 
+    managers;
+
+    // Checking which form the submit is from.
     onSubmit(person): void {
         console.log(person);
+        if (this.whichForm === 'person') {
+            console.log('Add Person');
+            this.addPerson(person);
+        }else if (this.whichForm === 'manager'){
+            console.log('Add Manager');
+            this.addManager(person);
+        }else{
+            console.log('Add Salesman');
+            this.addSalesman(person);
+        }
+
+    }
+
+    /**
+     * Functions for submiting the form and posting it to the API.
+     * Function calls from OnSubmit().
+     * @param person - JSON Object with person details for POST from form.
+     */
+    addPerson(person): void {
         console.log(this.dataService.postPerson(person));
+        this.router.navigate(['/home']);
+    }
+
+    addManager(person): void {
+        console.log(this.dataService.postManager(person));
+        this.router.navigate(['/home']);
+    }
+
+    addSalesman(person): void {
+        console.log(this.dataService.postSalesman(person));
         this.router.navigate(['/home']);
     }
 
 
     ngOnInit(): void {
+        this.whichForm = this.route.snapshot.paramMap.get('whichForm');
+
+        console.log('This is the form you have: ' + this.whichForm);
+
+        // Gathering GENDERS for dynamic dropdown display.
         this.dataService.getGenders().subscribe(genders => {
             this.genders = genders;
             console.log(this.genders);
         });
 
+        // Gathering MANAGERS for dynamic dropdown display
+        this.dataService.getManagers().subscribe(managers => {
+            this.managers = managers;
+            console.log(this.managers);
+        });
+
+        // PERSON form declaration.
         this.personForm = new FormGroup({
             name: new FormControl('', Validators.required),
             sex: new FormControl('', Validators.required),
@@ -36,6 +86,33 @@ export class AddComponent implements OnInit {
             street: new FormControl('', Validators.required),
             city: new FormControl('', Validators.required),
             eircode: new FormControl('', [Validators.required, Validators.maxLength(7)])
+        });
+
+        // MANAGER form declaration.
+        this.managerForm = new FormGroup({
+            name: new FormControl('', Validators.required),
+            sex: new FormControl('', Validators.required),
+            number: new FormControl('', [Validators.required, Validators.pattern('^[0-9]*$')]),
+            street: new FormControl('', Validators.required),
+            city: new FormControl('', Validators.required),
+            eircode: new FormControl('', [Validators.required, Validators.maxLength(7)]),
+            branch: new FormControl('', Validators.required),
+            department: new FormControl('', Validators.required)
+        });
+
+        // SALESMAN form declaration.
+        this.salesmanForm = new FormGroup({
+            name: new FormControl('', Validators.required),
+            sex: new FormControl('', Validators.required),
+            number: new FormControl('', [Validators.required, Validators.pattern('^[0-9]*$')]),
+            street: new FormControl('', Validators.required),
+            city: new FormControl('', Validators.required),
+            eircode: new FormControl('', [Validators.required, Validators.maxLength(7)]),
+            working_hours: new FormControl(40),
+            manager: new FormControl('', Validators.required),
+            base_salary: new FormControl('', Validators.required),
+            commission: new FormControl('', Validators.required),
+            branch: new FormControl('', Validators.required)
         });
     }
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,8 +11,6 @@ import { DataService } from './data.service';
 import { MainTableComponent } from './table/main-table.component';
 import { AddComponent } from './add/add.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AddManagerComponent } from './add-manager/add-manager.component';
-import { AddSalesmanComponent } from './add-salesman/add-salesman.component';
 
 @NgModule({
     declarations: [
@@ -21,8 +19,6 @@ import { AddSalesmanComponent } from './add-salesman/add-salesman.component';
         EditComponent,
         MainTableComponent,
         AddComponent,
-        AddManagerComponent,
-        AddSalesmanComponent
     ],
     imports: [
         BrowserModule,
